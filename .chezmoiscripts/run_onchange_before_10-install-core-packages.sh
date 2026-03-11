fb388d88a99e99eb7209e1a51ec9c7a625b223f6
Atanas Kuzmanov <atkuzmanov@gmail.com>
2026-03-11 23:07:55 +0200

    Add chezmoiscripts install core packages v1.

--- .chezmoiscripts/run_onchange_before_10-install-core-packages.sh
+++ .chezmoiscripts/run_onchange_before_10-install-core-packages.sh
@@ -5,9 +5,36 @@ if ! command -v apt-get >/dev/null 2>&1; then
   exit 0
 fi
 
+export DEBIAN_FRONTEND=noninteractive
+
 sudo apt-get update
+
 sudo apt-get install -y \
+  age \
+  bash-completion \
+  build-essential \
+  ca-certificates \
   curl \
+  file \
+  gawk \
   git \
-  zsh \
-  age
+  gnupg \
+  jq \
+  less \
+  locales \
+  lsb-release \
+  make \
+  nano \
+  openssh-client \
+  passwd \
+  rsync \
+  software-properties-common \
+  tmux \
+  unzip \
+  wget \
+  xclip \
+  zip \
+  zsh
+
+
+
